252974e5cbd3ab942f7f8c23b6bd08caed58e1f2
Nguyen Huu Duc B2013529 <ducb2013529@student.ctu.edu.vn>
2024-04-13 22:25:57 +0700

    trang chu gif

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { RegisterForm } from '@/components/auth/register-form';
 import LoginFormFB from '@/components/ui/facebook';
 import Footer from '@/components/ui/footer';
+import Image from "next/image";
 
 import { register } from 'module';
 
@@ -16,10 +17,18 @@ const HomePage = () => {
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         allowFullScreen
       ></iframe> */}
-      <div className="absolute inset-0" style={{ width: "100%", height: "100%" }}>
-          <img
-            src="public/images/background.gif"
+      <div
+          className="absolute inset-0 image-fluid"
+          style={{ width: "100%", height: "120%" }}
+        >
+          <Image
+            src="/images/background.gif"
             alt="background image"
+            className="image-fluid"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            style={{ filter: "brightness(50%)" }}
           />
         </div>
       <div style={{ position: 'absolute', top: '60%', left: '40%', transform: 'translate(-50%, -40%)', color: 'white', fontSize: '90px', fontWeight: 'bold', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
@@ -28,7 +37,10 @@ const HomePage = () => {
       <div style={{position: 'absolute', top: '10%', right: '6.5%', paddingBottom:'60%'}}>
       <RegisterForm />
       </div>
+      <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+
       <Footer/>
+      </div>
     </div>
     
   );
